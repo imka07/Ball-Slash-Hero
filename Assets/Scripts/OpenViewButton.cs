c29c330018d9609115995c8ff0856a4c0c260211
Shokuev <93195583+imka07@users.noreply.github.com>
2024-09-11 13:52:23 +0300

    WebView

--- Assets/Scripts/OpenViewButton.cs
+++ Assets/Scripts/OpenViewButton.cs
@@ -13,22 +13,19 @@ public class OpenViewButton : MonoBehaviour
     {
         if (firebaseInitializer != null)
         {
-            // Подписываемся на событие инициализации Firebase
             firebaseInitializer.FirebaseInitialized += OnFirebaseInitialized;
         }
-
-        // Проверяем, есть ли уже сохранённая ссылка
         if (PlayerPrefs.HasKey(ConfigDataKey))
         {
             string savedUrl = PlayerPrefs.GetString(ConfigDataKey);
             if (!string.IsNullOrEmpty(savedUrl))
             {
-                ShowWebPage(savedUrl); // Открываем вебвью, если ссылка есть
+                ShowWebPage(savedUrl); 
             }
             else
             {
                 Debug.Log("Сохранённой ссылки нет, показываем игру.");
-                ShowGame(); // Если ссылки нет, показываем игру
+                ShowGame(); 
             }
         }
     }
@@ -37,42 +34,35 @@ public class OpenViewButton : MonoBehaviour
     {
         if (firebaseInitializer != null)
         {
-            // Отписываемся от события
             firebaseInitializer.FirebaseInitialized -= OnFirebaseInitialized;
         }
     }
 
-    // Метод вызывается, когда данные с Firebase загружены
     private void OnFirebaseInitialized(string configData)
     {
         if (!string.IsNullOrEmpty(configData))
         {
-            ShowWebPage(configData); // Показываем WebView, если есть данные
+            ShowWebPage(configData); 
         }
         else
         {
             Debug.Log("Нет данных для отображения WebView, показываем игру.");
-            ShowGame(); // Если данные пустые, показываем игру
+            ShowGame(); 
         }
     }
 
-    // Метод для отображения WebView
     public void ShowWebPage(string url)
     {
         _viewObject = new GameObject("WebView");
         _view = _viewObject.AddComponent<UniWebView>();
         _view.Load(url);
         _view.Frame = new Rect(0, 0, Screen.width, Screen.height);
-        _view.SetShowToolbar(false); // Скрываем тулбар, так как кнопок закрытия быть не должно
+        _view.SetShowToolbar(false); 
         _view.Show();
-
-        // WebView не закрывается, как указано в вашем алгоритме
     }
 
-    // Метод для отображения игры
     private void ShowGame()
     {
         Debug.Log("Показываем игру...");
-        // Здесь можно добавить логику для показа игрового интерфейса
     }
 }
