6134b8eb828f7eac7251cad9f122ea4b0d035ddd
Shokuev <93195583+imka07@users.noreply.github.com>
2024-09-11 15:14:41 +0300

    1.0.0

--- Assets/Scripts/OpenViewButton.cs
+++ Assets/Scripts/OpenViewButton.cs
@@ -3,30 +3,14 @@ using UnityEngine;
 public class OpenViewButton : MonoBehaviour
 {
     [SerializeField] private FirebaseInitializer firebaseInitializer;
-
     private UniWebView _view;
     private GameObject _viewObject;
-
-    private const string ConfigDataKey = "saved_config_data";
 
     private void Start()
     {
         if (firebaseInitializer != null)
         {
             firebaseInitializer.FirebaseInitialized += OnFirebaseInitialized;
-        }
-        if (PlayerPrefs.HasKey(ConfigDataKey))
-        {
-            string savedUrl = PlayerPrefs.GetString(ConfigDataKey);
-            if (!string.IsNullOrEmpty(savedUrl))
-            {
-                ShowWebPage(savedUrl); 
-            }
-            else
-            {
-                Debug.Log("Сохранённой ссылки нет, показываем игру.");
-                ShowGame(); 
-            }
         }
     }
 
@@ -42,12 +26,11 @@ public class OpenViewButton : MonoBehaviour
     {
         if (!string.IsNullOrEmpty(configData))
         {
-            ShowWebPage(configData); 
+            ShowWebPage(configData);
         }
         else
         {
-            Debug.Log("Нет данных для отображения WebView, показываем игру.");
-            ShowGame(); 
+            Debug.Log("Нет данных для отображения WebView");
         }
     }
 
@@ -57,12 +40,7 @@ public class OpenViewButton : MonoBehaviour
         _view = _viewObject.AddComponent<UniWebView>();
         _view.Load(url);
         _view.Frame = new Rect(0, 0, Screen.width, Screen.height);
-        _view.SetShowToolbar(false); 
+        _view.SetShowToolbar(true);
         _view.Show();
-    }
-
-    private void ShowGame()
-    {
-        Debug.Log("Показываем игру...");
     }
 }
